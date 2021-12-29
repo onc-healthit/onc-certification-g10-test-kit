5fd82d405d8adddfa24581e5a2910a380c96a34a
Stephen MacVicar <smacvicar@mitre.org>
2021-12-29 15:39:53 -0500

    add unauthorized access test

--- lib/g10_certification_test_kit/smart_standalone_patient_app_group.rb
+++ lib/g10_certification_test_kit/smart_standalone_patient_app_group.rb
@@ -203,6 +203,30 @@ module G10CertificationTestKit
           end
         end
       end
+
+      test do
+        title 'Server rejects unauthorized access'
+        description %(
+          A server SHALL reject any unauthorized requests by returning an HTTP
+          401 unauthorized response code.
+        )
+        input :patient_id, name: :standalone_patient_id
+        input :url
+        uses_request :token
+
+        fhir_client :standalone_unauthenticated do
+          url :url
+        end
+
+        run do
+          skip_if request.status != 200, 'Token exchange was unsuccessful'
+          skip_if patient_id.blank?, 'Patient context expected to verify unauthorized read.'
+
+          fhir_read(:patient, patient_id, client: :standalone_unauthenticated)
+
+          assert_response_status(401)
+        end
+      end
     end
 
     group from: :smart_openid_connect,
