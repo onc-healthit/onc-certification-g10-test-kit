ad97e7f3ced110865e270c2b0acabc855d88d32c
Stephen MacVicar <smacvicar@mitre.org>
2022-01-27 11:53:02 -0500

    fix code checking task

--- lib/inferno/terminology/tasks/validate_code.rb
+++ lib/inferno/terminology/tasks/validate_code.rb
@@ -1,9 +1,13 @@
 require 'colorize'
+require_relative '../terminology_validation'
+require_relative '../loader'
 
 module Inferno
   module Terminology
     module Tasks
       class ValidateCode
+        include TerminologyValidation
+
         attr_reader :system, :code, :value_set_url
 
         def initialize(code:, system:, valueset:)
@@ -13,15 +17,16 @@ module Inferno
         end
 
         def run
+          Inferno::Terminology::Loader.load_validators
           code_display = self.system ? "#{self.system}|#{code}" : code.to_s
-          if Loader.validate_code(code: code, system: self.system, valueset_url: value_set_url)
+          if validate_code(code: code, system: self.system, value_set_url: value_set_url)
             in_system = 'is in'
             symbol = "\u2713".encode('utf-8').to_s.green
           else
             in_system = 'is not in'
             symbol = 'X'.red
           end
-          system_checked = valueset || self.system
+          system_checked = value_set_url || self.system
 
           Inferno.logger.info "#{symbol} #{code_display} #{in_system} #{system_checked}"
         end
