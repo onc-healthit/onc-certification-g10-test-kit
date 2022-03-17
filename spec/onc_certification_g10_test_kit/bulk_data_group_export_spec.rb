afb21ab45083a6d06613f32e1849c1ddacd9861c
Chris Dillon <cdillon@mitre.org>
2022-03-17 19:29:11 +0000

    Fix spec on feedback

--- spec/onc_certification_g10_test_kit/bulk_data_group_export_spec.rb
+++ spec/onc_certification_g10_test_kit/bulk_data_group_export_spec.rb
@@ -247,9 +247,7 @@ RSpec.describe ONCCertificationG10TestKit::BulkDataGroupExport do
       expect(result.result_message).to eq('Complete Status response did not contain "transactionTime" as required')
     end
 
-    it 'passes when server returns "202 
-    
-    ed" and response body contains required attributes' do
+    it 'passes when server returns "202 Accepted" and response body contains required attributes' do
       stub_request(:get, polling_url.to_s)
         .with(headers: { 'Authorization' => "Bearer #{bearer_token}" })
         .to_return(status: 200, body: status_response, headers: headers)
@@ -261,8 +259,10 @@ RSpec.describe ONCCertificationG10TestKit::BulkDataGroupExport do
 
     it 'sends content type headers' do
       get_stub = stub_request(:get, polling_url.to_s)
-        .with(headers: { 'Authorization' => "Bearer #{bearer_token}" })
-        .with(headers: { 'Accept' => 'application/json' })
+        .with(headers: {
+          'Authorization' => "Bearer #{bearer_token}",
+          'Accept' => 'application/json'
+        })
 
       result = run(runnable, input)
 
