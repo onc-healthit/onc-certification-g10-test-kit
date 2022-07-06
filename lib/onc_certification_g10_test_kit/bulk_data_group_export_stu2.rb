7306802306e19497d2b5797f7990ce402259dcf8
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-07-06 13:47:43 -0400

    wrap description/link

--- lib/onc_certification_g10_test_kit/bulk_data_group_export_stu2.rb
+++ lib/onc_certification_g10_test_kit/bulk_data_group_export_stu2.rb
@@ -14,7 +14,13 @@ module ONCCertificationG10TestKit
     test do
       title 'Bulk Data Server supports "_outputFormat" query parameter'
       description <<~DESCRIPTION
-        _outputFormat: The format for the requested Bulk Data files to be generated as per FHIR Asynchronous Request Pattern. Defaults to application/fhir+ndjson. The server SHALL support Newline Delimited JSON, but MAY choose to support additional output formats. The server SHALL accept the full content type of application/fhir+ndjson as well as the abbreviated representations application/ndjson and ndjson.
+        [_outputFormat](https://hl7.org/fhir/uv/bulkdata/export.html#query-parameters):
+        The format for the requested Bulk Data files to be
+        generated as per FHIR Asynchronous Request Pattern. Defaults to
+        application/fhir+ndjson. The server SHALL support Newline Delimited
+        JSON, but MAY choose to support additional output formats. The server
+        SHALL accept the full content type of application/fhir+ndjson as well
+        as the abbreviated representations application/ndjson and ndjson.
       DESCRIPTION
 
       include ExportKickOffPerformer
