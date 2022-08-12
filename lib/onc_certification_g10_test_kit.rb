19612f172a7b908fd84018d5e2aa045039f77f92
Stephen MacVicar <smacvicar@mitre.org>
2022-08-12 10:08:49 -0400

    put require behind feature flag

--- lib/onc_certification_g10_test_kit.rb
+++ lib/onc_certification_g10_test_kit.rb
@@ -3,8 +3,10 @@ require_relative 'onc_certification_g10_test_kit/feature'
 require 'smart_app_launch/smart_stu1_suite'
 require 'smart_app_launch/smart_stu2_suite' if ONCCertificationG10TestKit::Feature.smart_v2?
 require 'us_core_test_kit/generated/v3.1.1/us_core_test_suite'
-require 'us_core_test_kit/generated/v4.0.0/us_core_test_suite' if ONCCertificationG10TestKit::Feature.us_core_v4?
-require 'us_core_test_kit/generated/v5.0.1/us_core_test_suite'
+if ONCCertificationG10TestKit::Feature.us_core_v4?
+  require 'us_core_test_kit/generated/v4.0.0/us_core_test_suite'
+  require 'us_core_test_kit/generated/v5.0.1/us_core_test_suite'
+end
 
 require_relative 'onc_certification_g10_test_kit/configuration_checker'
 require_relative 'onc_certification_g10_test_kit/version'
