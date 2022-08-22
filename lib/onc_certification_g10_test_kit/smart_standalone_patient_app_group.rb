a455974c480f2c7187e3c536f6c5b7c3640d49a4
Stephen MacVicar <smacvicar@mitre.org>
2022-08-22 09:37:57 -0400

    update default standalone launch scopes for smart v2

--- lib/onc_certification_g10_test_kit/smart_standalone_patient_app_group.rb
+++ lib/onc_certification_g10_test_kit/smart_standalone_patient_app_group.rb
@@ -126,6 +126,25 @@ module ONCCertificationG10TestKit
           Sequence](http://hl7.org/fhir/smart-app-launch/1.0.0/index.html#standalone-launch-sequence)
       )
 
+      config(
+        inputs: {
+          requested_scopes: {
+            default: %(
+              launch/patient openid fhirUser offline_access
+              patient/Medication.read patient/AllergyIntolerance.read
+              patient/CarePlan.read patient/CareTeam.read patient/Condition.read
+              patient/Device.read patient/DiagnosticReport.read
+              patient/DocumentReference.read patient/Encounter.read
+              patient/Goal.read patient/Immunization.read patient/Location.read
+              patient/MedicationRequest.read patient/Observation.read
+              patient/Organization.read patient/Patient.read
+              patient/Practitioner.read patient/Procedure.read
+              patient/Provenance.read patient/PractitionerRole.read
+              ).gsub(/\s{2,}/, ' ').strip
+          }
+        }
+      )
+
       test from: :g10_smart_scopes do
         config(
           inputs: {
@@ -201,6 +220,25 @@ module ONCCertificationG10TestKit
 
           * [Standalone Launch
             Sequence](http://hl7.org/fhir/smart-app-launch/STU2/app-launch.html#launch-app-standalone-launch)
+        )
+
+        config(
+          inputs: {
+            requested_scopes: {
+              default: %(
+                launch/patient openid fhirUser offline_access
+                patient/Medication.rs patient/AllergyIntolerance.rs
+                patient/CarePlan.rs patient/CareTeam.rs patient/Condition.rs
+                patient/Device.rs patient/DiagnosticReport.rs
+                patient/DocumentReference.rs patient/Encounter.rs
+                patient/Goal.rs patient/Immunization.rs patient/Location.rs
+                patient/MedicationRequest.rs patient/Observation.rs
+                patient/Organization.rs patient/Patient.rs
+                patient/Practitioner.rs patient/Procedure.rs
+                patient/Provenance.rs patient/PractitionerRole.rs
+              ).gsub(/\s{2,}/, ' ').strip
+            }
+          }
         )
 
         test from: :g10_smart_scopes do
