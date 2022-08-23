a8d6b1fbbc886508f62078c56ece51c0415670f3
Stephen MacVicar <smacvicar@mitre.org>
2022-08-23 13:11:34 -0400

    remove feature flags

--- lib/onc_certification_g10_test_kit/feature.rb
+++ lib/onc_certification_g10_test_kit/feature.rb
@@ -1,17 +1,10 @@
 module ONCCertificationG10TestKit
   module Feature
-    class << self
-      def us_core_v4?
-        ENV.fetch('US_CORE_4_ENABLED', 'false')&.casecmp?('true')
-      end
-
-      def bulk_data_v2?
-        ENV.fetch('BULk_DATA_V2_ENABLED', 'false')&.casecmp?('true')
-      end
-
-      def smart_v2?
-        ENV.fetch('SMART_2_ENABLED', 'false')&.casecmp?('true')
-      end
+    class << self # rubocop:disable Lint/EmptyClass
+      # This is how you can define feature flags to be used in the g10 test kit
+      # def us_core_v4?
+      #   ENV.fetch('US_CORE_4_ENABLED', 'false')&.casecmp?('true')
+      # end
     end
   end
 end
