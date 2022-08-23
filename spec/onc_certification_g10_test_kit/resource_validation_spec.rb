182b8e5f68b5de526ae260148132bf3e397a2af2
Yunwei Wang <>
2022-08-23 16:13:21 -0500

    add unit test

--- spec/onc_certification_g10_test_kit/resource_validation_spec.rb
+++ spec/onc_certification_g10_test_kit/resource_validation_spec.rb
@@ -17,7 +17,29 @@ RSpec.describe 'Resource Validation' do # rubocop:disable RSpec/DescribeClass
     expect(runnable.messages.length).to be_zero
   end
 
-  it 'excludes codings not in VS messages' do
+  it 'excludes CodeableConcept not in VS messages' do
+    stub_request(:post, "#{validator_url}/validate?profile=PROFILE")
+      .to_return(status: 200, body: operation_outcome_string2)
+
+    expect(runnable.resource_is_valid?(resource: resource, profile_url: 'PROFILE')).to be(true)
+    expect(runnable.messages.length).to be_zero
+  end
+
+  it 'excludes Coding not in VS messages' do
+    oo = FHIR::OperationOutcome.new(
+      issue: [
+        {
+          severity: 'error',
+          code: 'code-invalid',
+          details: {
+            text: 'The Coding provided (urn:oid:2.16.840.1.113883.6.238#2106-3) is not in the value set http://hl7.org/fhir/us/core/ValueSet/omb-race-category, and a code is required from this value set. (error message = Not in value set http://hl7.org/fhir/us/core/ValueSet/omb-race-category)'
+          },
+          expression: [
+            'Patient.extension[0].extension[0].value.ofType(Coding)'
+          ]
+       }
+      ]
+    )
     stub_request(:post, "#{validator_url}/validate?profile=PROFILE")
       .to_return(status: 200, body: operation_outcome_string2)
 
