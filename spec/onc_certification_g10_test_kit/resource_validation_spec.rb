8b4943004500937fb0e97862888ec1be98f27f03
Yunwei Wang <>
2022-08-24 08:58:48 -0500

    rubocop

--- spec/onc_certification_g10_test_kit/resource_validation_spec.rb
+++ spec/onc_certification_g10_test_kit/resource_validation_spec.rb
@@ -32,16 +32,19 @@ RSpec.describe 'Resource Validation' do # rubocop:disable RSpec/DescribeClass
           severity: 'error',
           code: 'code-invalid',
           details: {
-            text: 'The Coding provided (urn:oid:2.16.840.1.113883.6.238#2106-3) is not in the value set http://hl7.org/fhir/us/core/ValueSet/omb-race-category, and a code is required from this value set. (error message = Not in value set http://hl7.org/fhir/us/core/ValueSet/omb-race-category)'
+            text: 'The Coding provided (urn:oid:2.16.840.1.113883.6.238#2106-3) is not in the value set ' \
+                  'http://hl7.org/fhir/us/core/ValueSet/omb-race-category, and a code is required from ' \
+                  'this value set. (error message = Not in value set ' \
+                  'http://hl7.org/fhir/us/core/ValueSet/omb-race-category)'
           },
           expression: [
             'Patient.extension[0].extension[0].value.ofType(Coding)'
           ]
-       }
+        }
       ]
     )
     stub_request(:post, "#{validator_url}/validate?profile=PROFILE")
-      .to_return(status: 200, body: operation_outcome_string2)
+      .to_return(status: 200, body: oo.to_json)
 
     expect(runnable.resource_is_valid?(resource: resource, profile_url: 'PROFILE')).to be(true)
     expect(runnable.messages.length).to be_zero
