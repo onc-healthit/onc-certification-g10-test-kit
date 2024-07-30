fc9860836d35f7fa5740820fdab17ec371b4439a
Stephen MacVicar <smacvicar@mitre.org>
2024-07-30 11:36:09 -0400

    distinguish granular scope inputs

--- lib/onc_certification_g10_test_kit/smart_fine_grained_scopes_group.rb
+++ lib/onc_certification_g10_test_kit/smart_fine_grained_scopes_group.rb
@@ -64,187 +64,64 @@ module ONCCertificationG10TestKit
       granular_scopes_group2.children << group
     end
 
-    # config(
-    #   inputs: {
-    #     client_secret: {
-    #       optional: false,
-    #       name: :standalone_client_secret
-    #     },
-    #     requested_scopes: {
-    #       name: :v1_requested_scopes,
-    #       default: %(
-    #         launch/patient openid fhirUser offline_access
-    #         patient/Medication.read patient/AllergyIntolerance.read
-    #         patient/CarePlan.read patient/CareTeam.read patient/Condition.read
-    #         patient/Device.read patient/DiagnosticReport.read
-    #         patient/DocumentReference.read patient/Encounter.read
-    #         patient/Goal.read patient/Immunization.read patient/Location.read
-    #         patient/MedicationRequest.read patient/Observation.read
-    #         patient/Organization.read patient/Patient.read
-    #         patient/Practitioner.read patient/Procedure.read
-    #         patient/Provenance.read patient/PractitionerRole.read
-    #       ).gsub(/\s{2,}/, ' ').strip
-    #     },
-    #     received_scopes: { name: :v1_received_scopes },
-    #     smart_credentials: { name: :v1_smart_credentials }
-    #   },
-    #   outputs: {
-    #     received_scopes: { name: :v1_received_scopes },
-    #     patient_id: { name: :v1_patient_id }
-    #   }
-    # )
+    config(
+      inputs: {
+        authorization_method: {
+          name: :granular_scopes_authorization_method,
+          title: 'Granular Scopes Authorization Request Method'
+        },
+        client_auth_type: {
+          name: :granular_scopes_client_auth_type,
+          title: 'Granular Scopes Client Authentication Type'
+        }
+      }
+    )
 
-    # input_order :url,
-    #             :standalone_client_id,
-    #             :standalone_client_secret,
-    #             :v1_requested_scopes,
-    #             :use_pkce,
-    #             :pkce_code_challenge_method,
-    #             :standalone_authorization_method,
-    #             :client_auth_type,
-    #             :client_auth_encryption_method
+    granular_scopes_group1.config(
+      inputs: {
+        client_id: {
+          name: :granular_scopes1_client_id,
+          title: 'Granular Scopes Group 1 Client ID'
+        },
+        client_secret: {
+          name: :granular_scopes1_client_secret,
+          title: 'Granular Scopes Group 1 Client Secret'
+        },
+        requested_scopes: {
+          title: 'Granular Scopes Group 1 Scopes'
+        }
+      }
+    )
 
-    # group from: :smart_discovery_stu2 do
-    #   test from: 'g10_smart_well_known_capabilities',
-    #        config: {
-    #          options: {
-    #            required_capabilities: [
-    #              'launch-standalone',
-    #              'client-public',
-    #              'client-confidential-symmetric',
-    #              'client-confidential-asymmetric',
-    #              'sso-openid-connect',
-    #              'context-standalone-patient',
-    #              'permission-offline',
-    #              'permission-patient',
-    #              'authorize-post',
-    #              'permission-v2',
-    #              'permission-v1'
-    #            ]
-    #          }
-    #        }
-    # end
+    granular_scopes_group2.config(
+      inputs: {
+        client_id: {
+          name: :granular_scopes2_client_id,
+          title: 'Granular Scopes Group 2 Client ID'
+        },
+        client_secret: {
+          name: :granular_scopes2_client_secret,
+          title: 'Granular Scopes Group 2 Client Secret'
+        },
+        requested_scopes: {
+          title: 'Granular Scopes Group 2 Scopes'
+        }
+      }
+    )
 
-    # group from: :smart_standalone_launch_stu2,
-    #       config: {
-    #         inputs: {
-    #           use_pkce: {
-    #             default: 'true',
-    #             locked: true
-    #           },
-    #           pkce_code_challenge_method: {
-    #             locked: true
-    #           },
-    #           authorization_method: {
-    #             name: :standalone_authorization_method,
-    #             default: 'get',
-    #             locked: true
-    #           },
-    #           client_auth_type: {
-    #             locked: true,
-    #             default: 'confidential_symmetric'
-    #           }
-    #         },
-    #         outputs: {
-    #           smart_credentials: { name: :v1_smart_credentials }
-    #         }
-    #       } do
-    #   title 'Standalone Launch With Patient Scope'
-    #   description %(
-    #     # Background
+    input_order :url,
+                :granular_scopes1_client_id,
+                :requested_scopes_group1,
+                :granular_scopes_authorization_method,
+                :granular_scopes_client_auth_type,
+                :granular_scopes1_client_secret,
+                :client_auth_encryption_method,
+                :granular_scopes2_client_id,
+                :requested_scopes_group2,
+                :granular_scopes2_client_secret,
+                :use_pkce,
+                :pkce_code_challenge_method,
+                :patient_ids
 
-    #     The [Standalone
-    #     Launch Sequence](http://hl7.org/fhir/smart-app-launch/STU2/app-launch.html#launch-app-standalone-launch)
-    #     allows an app, like Inferno, to be launched independent of an
-    #     existing EHR session. It is one of the two launch methods described in
-    #     the SMART App Launch Framework alongside EHR Launch. The app will
-    #     request authorization for the provided scope from the authorization
-    #     endpoint, ultimately receiving an authorization token which can be used
-    #     to gain access to resources on the FHIR server.
-
-    #     # Test Methodology
-
-    #     Inferno will redirect the user to the the authorization endpoint so that
-    #     they may provide any required credentials and authorize the application.
-    #     Upon successful authorization, Inferno will exchange the authorization
-    #     code provided for an access token.
-
-    #     For more information on the #{title}:
-
-    #     * [Standalone Launch
-    #       Sequence](http://hl7.org/fhir/smart-app-launch/STU2/app-launch.html#launch-app-standalone-launch)
-    #   )
-
-    #   test from: :g10_smart_scopes do
-    #     config(
-    #       options: {
-    #         requested_scope_version: :v1,
-    #         received_scope_version: :any,
-    #         required_scope_type: 'patient',
-    #         required_scopes: ['openid', 'fhirUser', 'launch/patient', 'offline_access']
-    #       }
-    #     )
-    #   end
-
-    #   test from: :g10_unauthorized_access,
-    #        config: {
-    #          inputs: {
-    #            patient_id: { name: :v1_patient_id }
-    #          }
-    #        }
-
-    #   test from: :g10_patient_context,
-    #        config: {
-    #          inputs: {
-    #            patient_id: { name: :v1_patient_id },
-    #            smart_credentials: { name: :v1_smart_credentials }
-    #          }
-    #        }
-
-    #   tests[0].config(
-    #     outputs: {
-    #       incorrectly_permitted_tls_versions_messages: {
-    #         name: :auth_incorrectly_permitted_tls_versions_messages
-    #       }
-    #     }
-    #   )
-
-    #   tests[3].config(
-    #     outputs: {
-    #       incorrectly_permitted_tls_versions_messages: {
-    #         name: :token_incorrectly_permitted_tls_versions_messages
-    #       }
-    #     }
-    #   )
-    # end
-
-    # group from: :g10_unrestricted_resource_type_access,
-    #       config: {
-    #         inputs: {
-    #           received_scopes: { name: :v1_received_scopes },
-    #           patient_id: { name: :v1_patient_id },
-    #           smart_credentials: { name: :v1_smart_credentials }
-    #         }
-    #       }
-
-    # test from: :g10_incorrectly_permitted_tls_versions_messages_setup,
-    #      id: :g10_auth_incorrectly_permitted_tls_versions_messages_setup,
-    #      config: {
-    #        inputs: {
-    #          incorrectly_permitted_tls_versions_messages: {
-    #            name: :auth_incorrectly_permitted_tls_versions_messages
-    #          }
-    #        }
-    #      }
-
-    # test from: :g10_incorrectly_permitted_tls_versions_messages_setup,
-    #      id: :g10_token_incorrectly_permitted_tls_versions_messages_setup,
-    #      config: {
-    #        inputs: {
-    #          incorrectly_permitted_tls_versions_messages: {
-    #            name: :token_incorrectly_permitted_tls_versions_messages
-    #          }
-    #        }
-    #      }
   end
 end
