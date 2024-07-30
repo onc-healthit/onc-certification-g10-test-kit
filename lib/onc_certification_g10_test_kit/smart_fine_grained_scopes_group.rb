877ca8bb6b11bec8bf3cbdb4d609fde8cccd4198
Stephen MacVicar <smacvicar@mitre.org>
2024-07-30 12:31:44 -0400

    update description

--- lib/onc_certification_g10_test_kit/smart_fine_grained_scopes_group.rb
+++ lib/onc_certification_g10_test_kit/smart_fine_grained_scopes_group.rb
@@ -23,6 +23,45 @@ module ONCCertificationG10TestKit
       * `Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh`
     )
 
+    description <<~DESCRIPTION
+      These tests verify that the system under test supports:
+
+      > SMART v2 scope syntax for patient-level and user-level scopes to support
+        the “permission-v2” “SMART on FHIR® Capability”, including support for
+        finer-grained resource constraints using search parameters according to
+        section 3.0.2.3 of the implementation specification at § 170.215(c)(2) for
+        the “category” parameter for the following resources: (1) Condition
+        resource with Condition sub-resources Encounter Diagnosis, Problem List,
+        and Health Concern; and (2) Observation resource with Observation
+        sub-resources Clinical Test, Laboratory, Social History, SDOH, Survey, and
+        Vital Signs
+
+      Prior to running these tests, first run the Single Patient API tests using
+      resource-level scopes. This group contains two sets of granular scope
+      tests, each of which includes a SMART Standalone Launch followed by FHIR
+      API tests. The app launches require that a particular subset of the
+      required granular scopes be granted. The FHIR API tests then repeat all of
+      the queries from the original Single Patient API tests that were run using
+      resource-level scopes, and verify that only resources matching the current
+      granular scopes are returned. Each group requires a separate set of
+      granular scopes to be granted:
+
+      Group 1:
+      * `Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|encounter-diagnosis`
+      * `Condition.rs?category=http://hl7.org/fhir/us/core/CodeSystem/condition-category|health-concern`
+      * `Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|laboratory`
+      * `Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|social-history`
+
+      Group 2:
+      * `Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item`
+      * `Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-signs`
+      * `Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|survey`
+      * `Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh`
+
+      [Finer-grained resource constraints using search
+      parameters](https://hl7.org/fhir/smart-app-launch/STU2/scopes-and-launch-context.html#finer-grained-resource-constraints-using-search-parameters)
+    DESCRIPTION
+
     id :g10_smart_fine_grained_scopes
 
     def self.short_id
@@ -37,11 +76,13 @@ module ONCCertificationG10TestKit
     # ehr launch with just the standalone launch group
     granular_scopes_group1 = children.first
     granular_scopes_group1.children[0] = granular_scopes_group1.children.first.children.first
-    granular_scopes_group1.children[0].required
+    standalone_launch_group1 = granular_scopes_group1.children[0]
+    standalone_launch_group1.required
 
     granular_scopes_group2 = children.last
     granular_scopes_group2.children[0] = granular_scopes_group2.children.first.children.first
-    granular_scopes_group2.children[0].required
+    standalone_launch_group2 = granular_scopes_group2.children[0]
+    standalone_launch_group2.required
 
     # Move the granular scope API groups to the top level
     api_group1 = granular_scopes_group1.children.pop
@@ -56,6 +97,10 @@ module ONCCertificationG10TestKit
       granular_scopes_group2.children << group
     end
 
+    # Remove OIDC and refresh token tests
+    standalone_launch_group1.children.pop(2)
+    standalone_launch_group2.children.pop(2)
+
     config(
       inputs: {
         authorization_method: {
@@ -65,6 +110,9 @@ module ONCCertificationG10TestKit
         client_auth_type: {
           name: :granular_scopes_client_auth_type,
           title: 'Granular Scopes Client Authentication Type'
+        },
+        received_scopes: {
+          name: :standalone_received_scopes
         }
       }
     )
