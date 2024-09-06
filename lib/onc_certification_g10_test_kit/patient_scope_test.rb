34e887492c23a63cdc755cb18dcca806c6e5b924
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2024-09-06 10:27:45 -0400

    FI-3064: Fix v2 scope check in EHR Launch with Patient Scopes (#558)
    * add passing patient scope test specs
    
    * add spec for separate read and search scopes
    
    * support receiving read and search scopes separately
    
    * remove old spec

--- lib/onc_certification_g10_test_kit/patient_scope_test.rb
+++ lib/onc_certification_g10_test_kit/patient_scope_test.rb
@@ -16,14 +16,40 @@ module ONCCertificationG10TestKit
     id :g10_patient_scope
     input :received_scopes
 
+    def scope_version
+      config.options[:scope_version]
+    end
+
     run do
-      expected_scope = if config.options[:scope_version] == :v2
-                         'patient/Patient.rs'
-                       else
-                         'patient/Patient.read'
-                       end
-      assert received_scopes&.include?(expected_scope),
-             "#{expected_scope} scope was requested, but not received. Received: `#{received_scopes}`"
+      expected_scopes =
+        if scope_version == :v2
+          [
+            Regexp.new(scope_regex_string('patient/Patient.rs').gsub('.rs', '.r?s')),
+            Regexp.new(scope_regex_string('patient/Patient.rs').gsub('.rs', '.rs?'))
+          ]
+        else
+          [Regexp.new(scope_regex_string('patient/Patient.read'))]
+        end
+
+      received_scopes = self.received_scopes.split
+
+      unmatched_scopes =
+        expected_scopes.reject do |expected_scope|
+          received_scopes.any? { |received_scope| received_scope.match? expected_scope }
+        end
+
+      assert unmatched_scopes.blank?,
+             "No scope matching the following was received: `#{unmatched_scopes_string(unmatched_scopes)}`"
+    end
+
+    def scope_regex_string(scope)
+      "\\A#{Regexp.quote(scope)}\\z"
+    end
+
+    def unmatched_scopes_string(unmatched_scopes)
+      unmatched_scopes
+        .map { |scope| "`#{scope.source}`" }
+        .join(', ')
     end
   end
 end
