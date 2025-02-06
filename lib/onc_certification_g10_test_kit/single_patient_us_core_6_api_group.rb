b89831a21de0903386772f0563c7295f8da42900
Stephen MacVicar <smacvicar@mitre.org>
2025-02-06 15:22:04 -0500

    fix importing to not alter the imported groups

--- lib/onc_certification_g10_test_kit/single_patient_us_core_6_api_group.rb
+++ lib/onc_certification_g10_test_kit/single_patient_us_core_6_api_group.rb
@@ -166,26 +166,27 @@ module ONCCertificationG10TestKit
       end
     end
 
-    USCoreTestKit::USCoreV610::USCoreTestSuite.groups[1].groups.each do |group|
-      test_group = group.ancestors[1]
+    USCoreTestKit::USCoreV610::USCoreTestSuite
+      .groups
+      .find { |g| g.title == 'US Core FHIR API' }
+      .groups
+      .each do |group|
+        test_group = group.ancestors[1]
 
-      next if test_group.optional?
+        next if test_group.optional?
 
-      id = test_group.id
+        group(from: test_group.id, exclude_optional: true)
 
-      group_config = {}
-      if test_group.respond_to?(:metadata) &&
-         test_group.metadata.delayed? &&
-         !test_group.metadata.searchable_delayed_resource?
-        test_group.children.reject! do |child|
-          child.include?(USCoreTestKit::SearchTest) &&
-            !child.include?(USCoreTestKit::PractitionerAddressTest)
+        if test_group.respond_to?(:metadata) && # rubocop:disable Style/Next
+           test_group.metadata.delayed? &&
+           !test_group.metadata.searchable_delayed_resource?
+          groups.last.children.reject! do |child|
+            child.include?(USCoreTestKit::SearchTest) &&
+              !child.include?(USCoreTestKit::PractitionerAddressTest)
+          end
+          groups.last.config(options: { read_all_resources: true })
         end
-        group_config[:options] = { read_all_resources: true }
       end
-
-      group(from: id, exclude_optional: true, config: group_config)
-    end
 
     groups.first.description %(
       The Capability Statement test verifies a FHIR server's ability support the
