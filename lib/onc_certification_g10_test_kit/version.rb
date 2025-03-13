89f04daec625a4dd70f658832b7fdf268b5bf580
Yunwei Wang <>
2025-03-13 14:06:24 -0500

    Update last_updated date

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
   VERSION = '7.1.0'.freeze
-  LAST_UPDATED = '2025-03-11'.freeze # TODO: update next release
+  LAST_UPDATED = '2025-03-13'.freeze # TODO: update next release
 end
