a50319fefcb3cc94d6793822a7fdbdd2fca4cfef
Dylan Hall <dehall@mitre.org>
2025-04-03 08:49:12 -0400

    FI-3867: Bump validator to v1.0.65

--- lib/onc_certification_g10_test_kit/configuration_checker.rb
+++ lib/onc_certification_g10_test_kit/configuration_checker.rb
@@ -2,7 +2,7 @@ require_relative '../inferno/terminology/tasks/check_built_terminology'
 
 module ONCCertificationG10TestKit
   class ConfigurationChecker
-    EXPECTED_HL7_VALIDATOR_VERSION = '1.0.60'.freeze
+    EXPECTED_HL7_VALIDATOR_VERSION = '1.0.65'.freeze
     HL7_VALIDATOR_VERSION_KEY = 'validatorWrapperVersion'.freeze
 
     def configuration_messages
