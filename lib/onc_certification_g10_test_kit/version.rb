7ebbba77a78e6284891d5372bb6e00f22d5d15e7
yunwwang <yunwwang@gmail.com>
2025-06-10 10:21:03 -0500

    Release v7.2.3 (#669)
    Co-authored-by: Yunwei Wang <>

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
-  VERSION = '7.2.2'.freeze
-  LAST_UPDATED = '2025-05-13'.freeze # TODO: update next release
+  VERSION = '7.2.3'.freeze
+  LAST_UPDATED = '2025-06-10'.freeze # TODO: update next release
 end
