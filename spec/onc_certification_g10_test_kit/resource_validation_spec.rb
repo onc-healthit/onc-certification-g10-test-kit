cae98fa8b61599448e5d966e2dcb7bf727a90757
Yunwei Wang <>
2025-07-15 10:01:49 -0500

    Update filter for tx.fhir.org message

--- spec/onc_certification_g10_test_kit/resource_validation_spec.rb
+++ spec/onc_certification_g10_test_kit/resource_validation_spec.rb
@@ -56,5 +56,29 @@ RSpec.describe 'Resource Validation' do # rubocop:disable RSpec/DescribeClass
       expect(runnable.resource_is_valid?(resource:, profile_url: 'PROFILE')).to be(true)
       expect(runnable.messages.length).to be_zero
     end
+
+    it 'excludes https://tx.fhir.org/r4 message' do
+      operation_outcome = {
+        outcomes: [
+          {
+            issues: [
+              {
+                location: "DocumentReference.content[0].attachment.contentType",
+                message: "The System URI could not be determined for the code 'text/plain' in the ValueSet 'http://hl7.org/fhir/ValueSet/mimetypes|4.0.1': include #0 has system urn:ietf:bcp:13 which could not be found, and the server returned error [Error from https://tx.fhir.org/r4: The code System \"urn:ietf:bcp:13\" has a grammar, and cannot be enumerated directly]",
+                type: 'CODEINVALID',
+                level: 'ERROR'
+              }
+            ]
+          }
+        ],
+        sessionId: '7c0cb248-4dd9-4063-9ed9-03623bbe221a'
+      }
+
+      stub_request(:post, "#{validator_url}/validate")
+        .to_return(status: 200, body: operation_outcome.to_json)
+
+      expect(runnable.resource_is_valid?(resource:, profile_url: 'PROFILE')).to be(true)
+      expect(runnable.messages.length).to be_zero
+    end
   end
 end
