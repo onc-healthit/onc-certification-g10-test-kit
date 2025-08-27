6f70dd57ba37fb64ec71a3c71422ea23735fc59f
ljtucker <ljtucker@leaporbit.com>
2025-08-27 10:24:58 -0400

    Update date of release in version file

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
   VERSION = '7.2.6'.freeze
-  LAST_UPDATED = '2025-08-25'.freeze # TODO: update next release
+  LAST_UPDATED = '2025-08-27'.freeze # TODO: update next release
 end
