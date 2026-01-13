f010e15bc0c021bb8525da4186f764c93112d850
Karl Naden <86612928+karlnaden@users.noreply.github.com>
2026-01-13 12:19:51 -0500

    Release 7.2.8 (#734)
    * update changelog
    
    * bump versions of us-core, inferno-core, and g10

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
-  VERSION = '7.2.7'.freeze
-  LAST_UPDATED = '2025-08-27'.freeze # TODO: update next release
+  VERSION = '7.2.8'.freeze
+  LAST_UPDATED = '2026-01-13'.freeze # TODO: update next release
 end
