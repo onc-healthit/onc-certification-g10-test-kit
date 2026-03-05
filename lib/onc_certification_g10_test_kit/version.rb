4da1711b6ad74e023baad346ad939da965f26eb9
Arun <arunkumarhk@users.noreply.github.com>
2026-03-05 15:57:45 -0500

    Release 7.3.0 (#748)

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
-  VERSION = '7.2.9'.freeze
-  LAST_UPDATED = '2026-02-06'.freeze # TODO: update next release
+  VERSION = '7.3.0'.freeze
+  LAST_UPDATED = '2026-03-05'.freeze # TODO: update next release
 end
