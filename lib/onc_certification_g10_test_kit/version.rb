7a0b016e7278bf62a07ea1cbc10be8dd0f74d2b5
Lucas Tucker <128156413+ljtucker@users.noreply.github.com>
2026-07-13 11:07:13 -0400

    Update US Core Gem version (#761)
    * Update US Core Gem version
    
    * Bump G10 version

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
-  VERSION = '8.0.3'.freeze
-  LAST_UPDATED = '2026-07-07'.freeze # TODO: update next release
+  VERSION = '8.0.4'.freeze
+  LAST_UPDATED = '2026-07-13'.freeze # TODO: update next release
 end
