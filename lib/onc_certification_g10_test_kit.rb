467cbeee192b44a9c301e83b1536788f53d728ae
Karl Naden <karl@glenviewassociates.com>
2026-08-31 23:27:42 -0400

    update validator and core versions

--- lib/onc_certification_g10_test_kit.rb
+++ lib/onc_certification_g10_test_kit.rb
@@ -2,5 +2,6 @@ require 'smart_app_launch/smart_stu1_suite'
 require 'smart_app_launch/smart_stu2_suite'
 require 'smart_app_launch/smart_stu2_2_suite'
 require 'us_core_test_kit'
+require 'davinci_crd_test_kit'
 
 require_relative 'onc_certification_g10_test_kit/g10_certification_suite'
