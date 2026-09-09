b402280be5303d25720c06632930276bbcc00deb
Karl Naden <karl@glenviewassociates.com>
2026-09-08 23:40:38 -0400

    update release date

--- lib/onc_certification_g10_test_kit/version.rb
+++ lib/onc_certification_g10_test_kit/version.rb
@@ -1,4 +1,4 @@
 module ONCCertificationG10TestKit
   VERSION = '8.0.7'.freeze
-  LAST_UPDATED = '2026-09-03'.freeze # TODO: update next release
+  LAST_UPDATED = '2026-09-08'.freeze # TODO: update next release
 end
